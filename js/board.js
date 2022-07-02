7c5e5a06984f01d56ae59f01996dfc4c90ee636e
Principe Matias <principematias23@gmail.com>
2022-07-02 15:59:36 -0300

    Cambios en la tabla, codigo html, css y javascript

--- js/board.js
+++ js/board.js
@@ -17,20 +17,19 @@ const tabla = ((resultJson)=>{
     const contTable = document.getElementById('contTable');
     for(let i of resultJson['data']){
         contTable.innerHTML +=`
-        <tr class="table__tr">
-            <td class="table__td">${i.id}</td>
-            <td class="table__td">${i.name}</td>
-            <td class="table__td">${i.username}</td>
-            <td class="table__td">${i.email}</td>
-            <td class="table__td">${i.address.street}</td>
-            <td class="table__td">${i.address.suite}</td>
-            <td class="table__td">${i.address.city}</td>
-            <td class="table__td">${i.phone}</td>
-            <td class="table__td">${i.company.name}</td>
+        <tr class='table__tr'>
+            <td class='table__td'>${i.id}</td>
+            <td class='table__td'>${i.name}</td>
+            <td class='table__td'>${i.username}</td>
+            <td class='table__td'>${i.email}</td>
+            <td class='table__td'>${i.address.street}</td>
+            <td class='table__td'>${i.address.suite}</td>
+            <td class='table__td'>${i.address.city}</td>
+            <td class='table__td'>${i.phone}</td>
+            <td class='table__td'>${i.company.name}</td>
         </tr>`
     }
 });
-
 fetch('https://basic-server-one.vercel.app/users')
 .then((result)=>{
     return result.json();
