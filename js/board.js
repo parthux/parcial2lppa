44402ba5bb9691e0b624110c66dff73a90483bac
Principe Matias <principematias23@gmail.com>
2022-07-02 18:11:59 -0300

    Cambio nombre de indeces html

--- js/board.js
+++ js/board.js
@@ -1,7 +1,7 @@
 //cheack login
 const testlogin = JSON.parse(localStorage.getItem('logit'));
 if(testlogin == false){
-    window.location.assign(`${window.location.origin}/login.html`);
+    window.location.assign(`${window.location.origin}/index.html`);
 }
 //button logout
 const logoutUser = document.getElementById('iconLogout');
